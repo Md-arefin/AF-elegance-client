6470807b1dbdce4337260a34a3c7e0b4be7e1fc1
Md-arefin <ansariarefin617@gmail.com>
2023-10-01 20:11:41 +0600

    first commit

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Footer from '../components/Footer/Footer';
+import Navbar from '../components/Navbar/Navbar';
+
+const Main = () => {
+    return (
+        <div>
+            <Navbar />
+            <div className='min-h-[calc(100vh-10rem)]'>
+                <Outlet />
+            </div>
+            <Footer />
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/Routes/Router.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {createBrowserRouter,RouterProvider} from "react-router-dom";
+import ErrorPage from '../components/Error/ErrorPage';
+import Main from '../Layout/Main';
+import Home from '../Pages/Home/Home/Home';
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+      ],
+    },
+  ]);
+
+export default router;
