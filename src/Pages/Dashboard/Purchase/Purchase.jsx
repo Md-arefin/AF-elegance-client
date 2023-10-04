a4adc7b9d4cebb7966251043363912319590ef62
Md-arefin <ansariarefin617@gmail.com>
2023-10-04 17:59:57 +0600

    implemented add  review

--- src/Pages/Dashboard/Purchase/Purchase.jsx
+++ src/Pages/Dashboard/Purchase/Purchase.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Purchase = () => {
     return (
         <div>
-            <p className='text-center text-3xl'> THis is purchase</p>
+            <p className='text-center text-3xl'> This is purchase history page.</p>
         </div>
     );
 };
--- src/Pages/Home/Review/Review.jsx
+++ src/Pages/Home/Review/Review.jsx
@@ -1,9 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import { BiSolidQuoteSingleRight } from 'react-icons/bi';
+import { Rating } from '@smastrom/react-rating'
+import '@smastrom/react-rating/style.css'
+import "swiper/css/navigation";
+import "swiper/css";
 
 const Review = () => {
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/get-review')
+            .then(res => res.json())
+            .then(data => {
+                setReviews(data);
+            })
+    }, [])
     return (
         <div className='my-20'>
-            <p className='text-center text-5xl'>This is Review section</p>
+            <Swiper autoplay={{
+                delay: 3000,
+                disableOnInteraction: false,
+            }}
+                modules={[Autoplay]}
+                className="mySwiper">
+                {
+                    reviews?.map((review) =>
+                        <SwiperSlide key={review._id} className="w-full">
+                            <div className='flex flex-col items-center'>
+                                <div className='flex flex-col items-center justify-center'>
+                                    <div className='flex justify-center items-center'>
+                                        <BiSolidQuoteSingleRight className='text-center text-5xl text-slate-500'></BiSolidQuoteSingleRight>
+                                        <BiSolidQuoteSingleRight className='text-center text-5xl text-slate-500'></BiSolidQuoteSingleRight>
+                                    </div>
+                                    <div className='text-center my-5'>
+                                        <p className='font-bold text-xl md:text-2xl px-3'>
+                                            {review?.message}
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className='flex items-center justify-center gap-3 pl-20 w-96'>
+                                    <div className="avatar">
+                                        <div className="w-16 md:w-20 lg:w-24 rounded-full">
+                                            <img src={review.photoURL} />
+                                        </div>
+                                    </div>
+                                    <div className='mx-auto space-y-2'>
+                                        <p className='text-neutral-500 font-bold text-2xl'><Rating style={{ maxWidth: 150 }} value={review?.ratings} /> </p>
+                                        <h3 className='text-xl md:text-3xl w-[300px]'> {review?.username}</h3>
+                                        <p className='text-neutral-500 font-bold text-xs md:text-lg lg:text-xl'>
+                                            {review?.email}
+                                        </p>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                    )
+                }
+            </Swiper>
         </div>
     );
 };
