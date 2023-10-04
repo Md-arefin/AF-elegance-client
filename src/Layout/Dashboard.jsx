2ff3cdd3104d0a6d112335e06dc1ada9458edbb5
Md-arefin <ansariarefin617@gmail.com>
2023-10-04 12:00:35 +0600

    fixed dashboard

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -9,29 +9,20 @@ import { AuthContext } from '../provider/AuthProvider';
 const Dashboard = () => {
     const { user } = useContext(AuthContext);
     return (
-        <div className="drawer lg:drawer-open">
-            <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-            <div className="drawer-content flex flex-col  ">
-                {/* Page content here */}
-                <label htmlFor="my-drawer-2" className="btn drawer-button lg:hidden pr-72"><svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg></label>
-                <Outlet />
-
-            </div>
-            <div className="drawer-side">
-                <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
-                <ul className="menu p-4 w-80 min-h-full bg-base-300 text-base-content">
-                    {/* Sidebar content here */}
-                    <div className='mx-auto lg:mt-20 lg:mb-10'>
-                        <div className="avatar">
-                            <div className="w-24  rounded-full">
-                                <img src={user?.photoURL} />
-                            </div>
+        <div className='flex justify-between h-screen w-full'>
+            <div className='bg-gray-400 hidden lg:block lg:w-96 h-full px-5'>
+                <div className='lg:pl-20 lg:mt-20 lg:mb-10'>
+                    <div className="avatar">
+                        <div className="w-24 lg:ml-3 rounded-full">
+                            <img src={user?.photoURL} />
                         </div>
-                        <p className='py-2 text-center text-xl'>
-                            {user?.displayName}
-                        </p>
                     </div>
-                    <div>
+                    <p className='py-2 text-xl px-2'>
+                        {user?.displayName}
+                    </p>
+                </div>
+                <div>
+                    <ul>
                         <li>
                             <NavLink to="/" className='lg:text-xl btn flex justify-between items-center my-2'>
                                 <p>
@@ -64,9 +55,65 @@ const Dashboard = () => {
                                 </p>
                                 <img className='w-8' src={profileImg} alt="" /></NavLink>
                         </li>
-                    </div>
-                </ul>
+                    </ul>
+                </div>
+            </div>
 
+            {/* OUTLET */}
+            <div className='w-full'>
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-30 py-5 px-4 shadow bg-gray-400 rounded-box w-72">
+                        <li>
+                            <div className='flex flex-col items-center justify-center'>
+                                <div className="avatar">
+                                    <div className="w-16 rounded-full">
+                                        <img src={user?.photoURL} />
+                                    </div>
+                                </div>
+                                <p className='py-2 text-md px-2'>
+                                    {user?.displayName}
+                                </p>
+                            </div>
+                        </li>
+                        <li>
+                            <NavLink to="/" className='text-lg btn flex justify-between items-center my-2 pt-2'>
+                                <p>
+                                    Home
+                                </p>
+                                <img className='w-8' src={homeImg} alt="" /></NavLink>
+                        </li>
+
+                        <li>
+                            <NavLink to="/" className='text-lg btn flex justify-between items-center my-2  pt-2'>
+                                <p>
+                                    History
+                                </p>
+                                <img className='w-8' src={HistoryImg} alt="" /></NavLink>
+                        </li>
+
+                        <li>
+                            <NavLink to="/dashboard/review" className='text-lg btn flex justify-between items-center my-2  pt-2'>
+                                <p>
+                                    Review
+                                </p>
+                                <img className='w-8' src={reviewImg} alt="" />
+                            </NavLink>
+                        </li>
+
+                        <li>
+                            <NavLink to="/profile" className='text-lg btn flex justify-between items-center my-2  pt-2'>
+                                <p>
+                                    Profile
+                                </p>
+                                <img className='w-8' src={profileImg} alt="" /></NavLink>
+                        </li>
+
+                    </ul>
+                </div>
+                <Outlet />
             </div>
         </div>
     );
