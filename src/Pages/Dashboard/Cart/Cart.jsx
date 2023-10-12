59ad20e4ff0eb6e7894f8cc6601359f7fcbffdf8
Md-arefin <ansariarefin617@gmail.com>
2023-10-12 15:13:31 +0600

    implemented delete cart

--- src/Pages/Dashboard/Cart/Cart.jsx
+++ src/Pages/Dashboard/Cart/Cart.jsx
@@ -3,27 +3,43 @@ import useCart from '../../../components/Hooks/useCart';
 import { FaTrashAlt } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import cash from '../../../assets/images/icons/money2.png'
+import Swal from 'sweetalert2';
 
 const Cart = () => {
 
-    const [carts] = useCart();
+    const [carts, refetch] = useCart();
 
     const totalPrice = carts.reduce((sum, item) => item.totalAmount + sum, 0)
 
-    console.log(totalPrice)
-
     const handleDeleteItems = (id) => {
         console.log(id);
+        fetch(`http://localhost:5000/carts/${id}`, {
+            method: "DELETE"
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.deletedCount) {
+                    refetch();
+                    Swal.fire({
+                        position: 'center',
+                        icon: 'success',
+                        title: "Item delete successful",
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                }
+            })
 
     }
 
     return (
         <div>
-            <div className='flex items-center justify-between mx-20'>
+            <div className='flex items-center justify-center mx-20 gap-5'>
                 <p className=' text-3xl my-6 ml-5'>Total Product : {carts.length}</p>
 
                 <div className='flex items-center justify-between gap-10'>
-                    <p className=' text-3xl my-6 ml-5'>Total Price :$ <span className='font-sans font-bold'> {totalPrice}</span></p>
+                    <p className=' text-3xl my-6 ml-5'>Total Price :$ <span className='font-sans font-semibold'> {totalPrice.toFixed(2)}</span></p>
 
                     <Link className='btn bg-slate-300 flex justify-between'>
                         <p className='text-xl'> Pay</p>
@@ -39,7 +55,8 @@ const Cart = () => {
                         <tr className='text-lg md:text-xl bg-gray-300 text-black'>
                             <th>
                             </th>
-                            <th>Items Name</th>
+                            <th>Items</th>
+                            <th>Name</th>
                             <th>Quantity</th>
                             <th>Total Price</th>
                             <th>Delete Item</th>
@@ -54,24 +71,22 @@ const Cart = () => {
                                         {i + 1}
                                     </th>
                                     <td>
-                                        <div className="flex items-center space-x-3">
-                                            <div className="avatar">
-                                                <div className="mask mask-squircle w-full h-20">
-                                                    <img src={cart.image} alt="Avatar Tailwind CSS Component" />
-                                                </div>
-                                            </div>
-                                            <div>
-                                                <div className="font-bold text-2xl">{cart.dressTitle}</div>
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-full h-20">
+                                                <img src={cart.image} alt="Avatar Tailwind CSS Component" />
                                             </div>
                                         </div>
                                     </td>
-                                    <td className='text-xl'>
+                                    <td>
+                                        <div className="font-bold text-xl">{cart.dressTitle}</div>
+                                    </td>
+                                    <td className='text-xl text-center'>
                                         {cart.quantity}
                                     </td>
-                                    <td className='text-xl'>
+                                    <td className='text-xl text-right'>
                                         $ {cart.totalAmount}
                                     </td>
-                                    <td>
+                                    <td className='text-center'>
                                         <div onClick={() => handleDeleteItems(cart._id)} className='btn bg-red-600 text-white hover:text-black'>
                                             <FaTrashAlt />
                                         </div>
