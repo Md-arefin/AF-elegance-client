cd671c0aa2e07c69de9862c227d0755af5cb80df
Md-arefin <ansariarefin617@gmail.com>
2023-10-14 18:48:53 +0600

    implemented

--- src/Pages/Home/BestSells/BestSells.jsx
+++ src/Pages/Home/BestSells/BestSells.jsx
@@ -4,33 +4,29 @@ import { Link } from 'react-router-dom';
 
 const BestSells = () => {
 
-    const { data: allProducts = [], refetch } = useQuery(["allProducts"], async () => {
-        const res = await fetch('http://localhost:5000/all-products')
+    const { data: bestSales = [], refetch } = useQuery(["bestSales"], async () => {
+        const res = await fetch('http://localhost:5000/bestSales')
         return res.json();
     })
-
+console.log(bestSales);
     return (
         <div className='my-20'>
             <p className='text-center text-2xl lg:text-5xl px-4 my-10'> Stylish Dresses for Every Season</p>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 w-full gap-5 items-center px-2'>
                 {
-                    allProducts.map((product) =>
-                        <>
-                            {
-                                product.bestSales == "Yes" && <div key={product._id} className="card bg-base-100 shadow-xl lg:h-[500px] relative">
-                                    <figure><img src={product.image} className="" alt="Shoes" /></figure>
-                                    <div className="card-body">
-                                        <h2 className="card-title">{product.dressTitle}</h2>
-                                        <p className='text-2xl font-serif'><span>Price: $</span>{product.price}</p>
-                                        <div className="card-actions ml-12">
-                                        <Link to={`/product/${product._id}`} className="w-full">
-                                                <button className="btn btn-primary">View details</button>
-                                            </Link>
-                                        </div>
-                                    </div>
+                    bestSales.map((product) =>
+                        <div key={product._id} className="card bg-base-100 shadow-xl lg:h-[500px] relative">
+                            <figure><img src={product.image} className="" alt="Shoes" /></figure>
+                            <div className="card-body">
+                                <h2 className="card-title">{product.dressTitle}</h2>
+                                <p className='text-2xl font-serif'><span>Price: $</span>{product.price}</p>
+                                <div className="card-actions ml-12">
+                                    <Link to={`/product/${product._id}`} className="w-full">
+                                        <button className="btn btn-primary">View details</button>
+                                    </Link>
                                 </div>
-                            }
-                        </>
+                            </div>
+                        </div>
                     )
                 }
             </div>
--- src/Pages/Home/NewArrival/NewArrival.jsx
+++ src/Pages/Home/NewArrival/NewArrival.jsx
@@ -5,8 +5,8 @@ import sale from '../../../assets/images/icons/sale.png';
 
 const NewArrival = () => {
 
-    const { data: allProducts = [], refetch } = useQuery(["allProducts"], async () => {
-        const res = await fetch('http://localhost:5000/all-products')
+    const { data: sales = [], refetch } = useQuery(["sales"], async () => {
+        const res = await fetch('http://localhost:5000/sales')
         return res.json();
     })
 
@@ -17,29 +17,25 @@ const NewArrival = () => {
 
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 w-full gap-5 items-center px-2'>
                 {
-                    allProducts.map((product) =>
-                        <>
-                            {
-                                product.sales && <div key={product._id} className="card bg-base-100 shadow-xl lg:h-[500px] relative">
-                                    <figure><img src={product.image} className="" alt="Shoes" /></figure>
-                                    <div className="card-body">
-                                        <h2 className="card-title">{product.dressTitle}</h2>
+                    sales.map((product) =>
+                        <div key={product._id} className="card bg-base-100 shadow-xl lg:h-[500px] relative">
+                            <figure><img src={product.image} className="" alt="Shoes" /></figure>
+                            <div className="card-body">
+                                <h2 className="card-title">{product.dressTitle}</h2>
 
-                                        <div className='absolute top-2 right-4'>
-                                            <div className='relative'>
-                                                <img src={sale} className="w-28" alt="" />
-                                                <p className='absolute top-2 right-10 text-3xl text-white -rotate-[23deg] font-serif'>{product.sales}%</p>
-                                            </div>
-                                        </div>
-                                        <div className="card-actions ml-12">
-                                            <Link to={`/product/${product._id}`} className="w-full">
-                                                <button className="btn btn-primary">View details</button>
-                                            </Link>
-                                        </div>
+                                <div className='absolute top-2 right-4'>
+                                    <div className='relative'>
+                                        <img src={sale} className="w-28" alt="" />
+                                        <p className='absolute top-2 right-10 text-3xl text-white -rotate-[23deg] font-serif'>{product.sales}%</p>
                                     </div>
                                 </div>
-                            }
-                        </>
+                                <div className="card-actions ml-12">
+                                    <Link to={`/product/${product._id}`} className="w-full">
+                                        <button className="btn btn-primary">View details</button>
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
                     )
                 }
             </div>
